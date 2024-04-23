c8335f8c08241a73d729c6cebe4d364fdf03bbf7
Lu1ssD33V <167026544+Lu1ssD33V@users.noreply.github.com>
2024-04-22 19:23:39 -0600

    Create ItemArgument.php

---
+++ src/CortexPE/Commando/args/ItemArgument.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CortexPE\Commando\args;
+
+use CortexPE\Commando\args\StringEnumArgument;
+
+use pocketmine\item\Item;
+use pocketmine\item\StringToItemParser;
+use pocketmine\command\CommandSender;
+
+final class ItemArgument extends StringEnumArgument {
+
+	public function getTypeName() : string {
+		return "item";
+	}
+
+	public function canParse(string $testString, CommandSender $sender) : bool {
+		return $this->getValue($testString) instanceof Item;
+	}
+
+	public function parse(string $argument, CommandSender $sender) : ?Rank {
+		return $this->getValue($argument);
+	}
+
+	public function getValue(string $string) : ?Rank {
+		return StringToItemParser::getInstance()->parse($string);
+	}
+
+	public function getEnumValues() : array {
+		return StringToItemParser::getInstance()->getKnownAliases();
+	}
+
+}
